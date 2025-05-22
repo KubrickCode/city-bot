17bf0f2a7785b71470d9bc3647a50cfb2c407d89
kubrickcode <kubrickcode@gmail.com>
2025-05-22 09:44:37 +0000

    핸들러 폴더 분리

--- src/main.go
+++ src/main.go
@@ -3,15 +3,14 @@ package main
 import (
 	"log"
 
+	"github.com/KubrickCode/city-bot/src/server/handlers"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("시티봇 서버가 실행 중입니다!")
-	})
+	app.Get("/", handlers.Index)
 
 	err := app.Listen(":3000")
 	if err != nil {
